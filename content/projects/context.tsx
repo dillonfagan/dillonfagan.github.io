113a736de821d28b9f17688fa18a01202efa127b
Dillon Fagan <dillonfagan@pm.me>
2024-11-04 16:19:19 -0500

    Enrich project content

--- content/projects/context.tsx
+++ content/projects/context.tsx
@@ -26,12 +26,24 @@ export const ProjectsProvider = ({ children }: PropsWithChildren) => {
       },
       {
         title: 'Library OS',
-        summary: 'Everything you need to manage a library of things.'
+        summary: 'Software to simplify tool library management. Developed from scratch for PVD Things.',
+        url: 'https://www.pvdthings.coop/software',
       },
       {
         title: 'Mutual Credit',
-        summary: "Instant access to community credit and low transaction fees for local businesses. It's the future of digital finance for the cooperative age."
-      }
+        summary: "Instant access to community credit and low transaction fees for local businesses. It's the future of digital finance for the cooperative age.",
+        url: 'https://github.com/dillonfagan/mutual_credit_app',
+      },
+      {
+        title: 'Mob',
+        summary: "A timer for good mob programming. Installable as a progressive web application (PWA).",
+        url: '/mob/index.html',
+      },
+      {
+        title: 'Rhode Island Worker Cooperative Alliance',
+        summary: 'Educating the public about worker-owned cooperatives and building mutualist infrastructure in the Providence area, representing over 3,200 co-op members.',
+        url: 'https://riworkercoop.org/',
+      },
     ]
   };
 
--- views/Hero/Hero.tsx
+++ views/Hero/Hero.tsx
@@ -8,39 +8,37 @@ import Projects from "./Projects";
 export default () => {
   return (
     <ProjectsProvider>
-      <main>
-        <div className="flex flex-col gap-4 lg:gap-8 items-stretch mx-auto w-screen p-4 lg:p-8">
-          <Text.Heading h="h1" size="3xl">
-            Dillon Fagan
-          </Text.Heading>
-          <Card.Outlined>
-            <Card.Body>
-              <Text.Hero>
-                I <Text.Primary>develop</Text.Primary> software,
-                <br />
-                <Text.Primary>envision</Text.Primary> products,
-                <br />
-                and <Text.Primary>organize</Text.Primary> cooperative
-                enterprises.
-              </Text.Hero>
-            </Card.Body>
-          </Card.Outlined>
-          <SectionHeading>Projects</SectionHeading>
-          <Projects />
-          <SectionHeading>Technologies</SectionHeading>
-          <section className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-8 h-64 xl:h-36">
-            {tech.map(({ color, image, title }) => (
-              <Card key={`tech-${title}`} bg={color} relative>
-                <Card.Body>
-                  <Text.Heading size="xl">{title}</Text.Heading>
-                  <div className="absolute -bottom-1 -right-1">
-                    <Image src={image} alt={title} height={80} width={80} />
-                  </div>
-                </Card.Body>
-              </Card>
-            ))}
-          </section>
-        </div>
+      <main className="flex flex-col gap-4 lg:gap-8 items-stretch mx-auto w-screen p-4 lg:p-8">
+        <Text.Heading h="h1" size="3xl">
+          Dillon Fagan
+        </Text.Heading>
+        <Card.Outlined>
+          <Card.Body>
+            <Text.Hero>
+              I <Text.Primary>develop</Text.Primary> software,
+              <br />
+              <Text.Primary>envision</Text.Primary> products,
+              <br />
+              and <Text.Primary>organize</Text.Primary> cooperative
+              enterprises.
+            </Text.Hero>
+          </Card.Body>
+        </Card.Outlined>
+        <SectionHeading>Projects</SectionHeading>
+        <Projects />
+        <SectionHeading>Technologies</SectionHeading>
+        <section className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-8 h-64 xl:h-36">
+          {tech.map(({ color, image, title }) => (
+            <Card key={`tech-${title}`} bg={color} relative>
+              <Card.Body>
+                <Text.Heading size="xl">{title}</Text.Heading>
+                <div className="absolute -bottom-1 -right-1">
+                  <Image src={image} alt={title} height={80} width={80} />
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
+        </section>
       </main>
     </ProjectsProvider>
   );
